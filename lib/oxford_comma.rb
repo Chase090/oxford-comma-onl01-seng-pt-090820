bcce81dba1ba7d9697da926ccb226c2edced7e1f
Chase090 <carloyam21@icloud.com>
2020-11-07 23:00:49 -0800

    commit tree 35694988d520f0f6ee1da5e7dfa8ced899de00d4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,72 @@
-def oxford_comma(array)
-    if array.size == 1
-      array.join(", ")
-    elsif
-      array.size == 2
-      array.join(" and ")
-    else
-      array[-1] = "and " + array[-1]
-      array.join(", ")
-    end
+require 'pry'
+
+def oxford_comma(argument)
+  
+  if argument.size == 1
+  argument.join(", ")
+  elsif argument.size == 2
+    argument.join(" and ")
+  else
+    argument[-1] = "and " + argument[-1]
+    # else the last arguement = "and" + last array
+    # meaning "and" would be added before the array
+    argument.join(", ")
   end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def oxford_comma(array)
+#     if array.size == 1
+#       array.join(", ")
+#     elsif
+#       array.size == 2
+#       array.join(" and ")
+#     else
+#       array[-1] = "and " + array[-1]
+#       array.join(", ")
+#     end
+#   end
